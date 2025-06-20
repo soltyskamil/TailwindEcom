75754bf713b95c3a6f54356e5ccc8d895c272d5a
Kamil <kamils3542@gmail.com>
2025-06-20 11:41:33 +0200

    infinity carousel

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,238 +1,115 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { store } from "../../store/main-store";
-import { productsSlice } from "../../store/products-reducer";
 import ProductCard from "../ProductCard/ProductCard";
-import { Carousel } from "react-responsive-carousel";
-import Slider from "react-slick";
-import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
-import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
-import { motion, useMotionValue, useAnimation } from "framer-motion";
-import type { PanInfo } from "framer-motion";
-import { useWindowSize } from "@uidotdev/usehooks";
-import { debounce } from "lodash";
+import {
+  motion,
+  useMotionValue,
+  useAnimation,
+  type PanInfo,
+  animate,
+  useAnimate,
+} from "framer-motion";
+
+import { useMeasure, useWindowSize } from "@uidotdev/usehooks";
+import { debounce, repeat } from "lodash";
 import "./ProductsList.css";
 const ProductsList = () => {
-  /**
-   * Mainstate
-   */
   const { products } = store.getState().productsSliceReducer;
-  const [index, setIndex] = useState(1);
-  const [prevIndex, setPrevIndex] = useState(0);
-  const [chunkSize, setChunkSize] = useState(4);
-  const [maxIndex, setMaxIndex] = useState(products.length / chunkSize);
-  const [dragged, setDragged] = useState(false);
-  const [width, setWidth] = useState(0);
-  const size = useWindowSize();
   const [constraintLeft, setConstraintLeft] = useState(0);
   const [node, setNode] = useState<HTMLElement>();
-
-  /**
-   * Breakpoints
-   */
-  const [isDesktopSmall, setIsSmallDesktop] = useState(
-    window.innerWidth < 1440 && window.innerWidth > 840
-  );
-  const [isTablet, setIsTablet] = useState(
-    window.innerWidth < 840 && window.innerWidth > 540
-  );
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 540);
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1440);
-
-  /**
-   * Refs
-   */
   const carouselRef = useRef<HTMLDivElement>(null);
-  const productsContainer = useRef<HTMLDivElement>(null);
   const productRef = useRef<HTMLDivElement>(null);
-  const constraintRef = useRef<HTMLDivElement>(null);
-
-  /**
-   * Hooks
-   */
-  const x = useMotionValue(0);
-  const controls = useAnimation();
-
-  /**
-   * Next/previous chunk handler
-   */
-  const handleNextChunk = () => {
-    if (index === maxIndex - 1) return;
-    else {
-      setDragged(false);
-      setPrevIndex(index);
-      setIndex((prev) => prev + 1);
-    }
-  };
-
-  const handlePreviousChunk = () => {
-    if (index === 1) return;
-    else {
-      setDragged(false);
-      setPrevIndex(index);
-      setIndex((prev) => prev - 1);
-    }
-  };
-
-  // useEffect(() => {
-  //   x.set(x.get() + 2);
-  //   // console.log(x.get());
-  // }, [size]);
-
-  /**
-   * Handles user drag and sets correct index based on it
-   */
-  const handleDrag = (e: MouseEvent | TouchEvent, info: PanInfo) => {
-    console.log(x.get());
-    setDragged(true);
-    // if (!productsContainer.current) return;
-    // const styles = getComputedStyle(productsContainer.current);
-    // const value = Number(styles.getPropertyValue("transform").split(",")[4]);
-    // console.log(value);
-
-    // if (value > -1300) {
-    //   setIndex(2);
-    // }
-    // if (value >= -2600 && value <= -1300) {
-    //   setIndex(2);
-    // }
-    // if (value >= -3900 && value <= -2600) {
-    //   setIndex(3);
-    // }
-    // if (value >= -5550 && value <= -3900) {
-    //   setIndex(4);
-    // }
-    // if (value <= -5550) {
-    //   setIndex(5);
-    // }
-  };
-
-  /**
-   * UseEffect responsible for responsiveness
-   */
-
-  // console.log("dd");
-  const [offset, setOffset] = useState(0);
-  useEffect(() => {
-    if (!node) return;
-    if (!carouselRef.current) return;
-    const scrollableWidth =
-      carouselRef.current.scrollWidth - carouselRef.current.offsetWidth;
-    setWidth(scrollableWidth);
-
-    setOffset((prev) => prev++);
-
-    setIsDesktop(window.innerWidth > 1440);
-    setIsSmallDesktop(window.innerWidth < 1440 && window.innerWidth > 840);
-    setIsTablet(window.innerWidth < 840 && window.innerWidth > 540);
-    setIsMobile(window.innerWidth < 540);
-  }, [size]);
-
-  // useEffect(() => {
-  //   console.log(width);
-  // }, [width]);
-
-  /**
-   * UseEffect responsible for setting correct chunk size
-   */
-  useEffect(() => {
-    if (isDesktop) {
-      setChunkSize(4);
-    }
-    if (isDesktopSmall) {
-      setChunkSize(3);
-    }
-    if (isTablet) {
-      setChunkSize(2);
-    }
-    if (isMobile) {
-      setChunkSize(1);
-    }
-  }, [size]);
-
-  /**
-   * Useeffect responsible for setting max index
-   */
-  useEffect(() => {
-    if (isDesktopSmall) {
-      setMaxIndex(Math.round(products.length / chunkSize));
-    } else setMaxIndex(products.length / chunkSize);
-  }, [chunkSize]);
-
-  /**
-   * Useeffect responsible for animation handling
-   */
-  useEffect(() => {
-    if (!carouselRef.current || !productRef.current) return;
-    if (dragged) return;
-    const productWidth = productRef.current.offsetWidth + 16;
-
-    // if (index === 1 && prevIndex === 0) return;
-
-    if (prevIndex < index) {
-      if (index === 1) return;
-      controls.start({
-        x: chunkSize * index * -productWidth,
-      });
-    }
-    if (prevIndex > index) {
-      controls.start({
-        x: chunkSize * index * -productWidth + chunkSize * productWidth,
-      });
-    }
-  }, [index, prevIndex]);
+  const size = useWindowSize();
+  // const controls = useAnimation();
+  const xTranslation = useMotionValue(0);
+  const FAST_DURATION = 20;
+  const SLOW_DURATION = 75;
+  const [duration, setDuration] = useState(FAST_DURATION);
+  const [mustFinish, setMustFinish] = useState(false);
+  const [rerender, setRerender] = useState(false);
 
   const containerRef = useCallback((node: HTMLElement | null) => {
     if (!node) return;
     setNode(node);
   }, []);
+
   useEffect(() => {
     if (!node) return;
-
     const handleResize = debounce(() => {
       setConstraintLeft(-(node.scrollWidth - node.clientWidth));
     }, 500);
-
     handleResize();
-
     const resizeObserver = new ResizeObserver(handleResize);
     resizeObserver.observe(node);
 
     return () => resizeObserver.disconnect();
   }, [node]);
 
+  useEffect(() => {
+    setRerender(!rerender);
+  }, [size]);
+
+  // useEffect(() => {
+  //   console.log(constraintLeft);
+  // }, [constraintLeft]);
+
+  useEffect(() => {
+    if (constraintLeft === 0 || constraintLeft == -0) return;
+    let controls;
+    if (mustFinish) {
+      controls = animate(xTranslation, [xTranslation.get(), constraintLeft], {
+        ease: "linear",
+        duration: duration * (1 - xTranslation.get() / constraintLeft),
+        onComplete: () => {
+          setMustFinish(false);
+          setRerender(!rerender);
+        },
+      });
+    } else {
+      console.log("else");
+      controls = animate(xTranslation, [0, constraintLeft], {
+        ease: "linear",
+        duration: duration,
+        repeat: Infinity,
+        // repeatType: "loop",
+      });
+    }
+
+    return controls?.stop;
+  }, [duration, xTranslation, mustFinish, constraintLeft]);
+
+  useEffect(() => {
+    xTranslation.set(0);
+  }, [size]);
+
   return (
-    <div className="products-wrapper  relative p-20 max-[1024px]:p-8">
-      <button
-        onClick={handlePreviousChunk}
-        // key={JSON.stringify(size.width)}
-        className="border p-4 absolute top-1/2 left-2"
-      >
-        <ArrowBackIosNewOutlinedIcon fontSize="large" />
-      </button>
-      <button
-        onClick={handleNextChunk}
-        className="border p-4 absolute top-1/2 right-2"
-      >
-        <ArrowForwardIosOutlinedIcon fontSize="large" />
-      </button>
+    <div className="products-wrapper  py-20 max-[1024px]:p-8">
       <div className="carousel-wrapper overflow-hidden" ref={carouselRef}>
         <motion.div
+          // transition={{ repeat: Infinity }}
           drag="x"
-          onDragEnd={handleDrag}
-          // dragConstraints={constraintRef}
+          style={{ x: xTranslation }}
+          // animate={{ x: constraintLeft }}
           dragConstraints={{ right: 0, left: constraintLeft }}
-          // dragConstraints={{ right: 0, left: -width }}
-          style={{ x }}
-          // style={{ translateX: 0 }} backup
-          animate={controls}
+          // style={{ translateX: 0 }}
           id="products"
           ref={containerRef}
-          key={JSON.stringify(size.width)}
-          className="product-list flex"
+          onHoverStart={() => {
+            setMustFinish(true);
+            setDuration(SLOW_DURATION);
+          }}
+          onHoverEnd={() => {
+            setMustFinish(true);
+            setDuration(FAST_DURATION);
+          }}
+          key={JSON.stringify(rerender) + JSON.stringify(size)}
+          className="product-list left-0 flex"
         >
-          {products.map((product: any, idx: number) => (
-            <ProductCard {...product} key={idx} ref={productRef} />
-          ))}
+          {[...products, ...products.slice(0, 4)].map(
+            (product: any, idx: number) => (
+              <ProductCard {...product} key={idx} ref={productRef} />
+            )
+          )}
         </motion.div>
       </div>
     </div>
